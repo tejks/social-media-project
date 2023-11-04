1275d759266e2404291f2bfa3da50365ad33860d
tejks <sarmatys.programmer@gmail.com>
2023-11-04 15:12:46 +0100

    feat: Create authSlice and configure redux

---
+++ src/common/store/index.ts
@@ -0,0 +1,28 @@
+import { ConfigureStoreOptions, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import { authApi } from '../API/services/auth';
+import { postApi } from '../API/services/post';
+import { commentApi } from '../API/services/comment';
+
+import auth from './authSlice';
+
+export const createStore = (options?: ConfigureStoreOptions['preloadedState'] | undefined) =>
+  configureStore({
+    reducer: {
+      [postApi.reducerPath]: postApi.reducer,
+      [commentApi.reducerPath]: commentApi.reducer,
+      [authApi.reducerPath]: authApi.reducer,
+      auth,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(postApi.middleware, commentApi.middleware, authApi.middleware),
+    ...options,
+  });
+
+export const store = createStore();
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type RootState = ReturnType<typeof store.getState>;
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,11 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import router from './pages/Router.tsx';
+import { store } from './common/store/index.ts';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
