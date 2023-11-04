3b27b182751d788c20926f9f893ed52ed5a9fc57
tejks <sarmatys.programmer@gmail.com>
2023-11-04 15:06:58 +0100

    fix: Move Error page to components file

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, Outlet } from 'react-router-dom';
 import Home from './Home';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import ErrorPage from '../ErrorPage';
+import ErrorPage from '../components/ErrorPage';
 
 const router = createBrowserRouter([
   {
