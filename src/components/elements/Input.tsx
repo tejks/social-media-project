db27aa700a79cadd7d3fbec1d7c6119823f8fb3e
jaugustyn <kub4a1@gmail.com>
2023-11-05 23:05:37 +0100

    fix: Added wrapper

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,22 +1,24 @@
 import clsx from 'clsx';
+import FieldWrapper, { WrapperPassThroughProps } from './FieldWrapper';
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-    type?: "text" | "number" | "email" | "password";
-    className?: string;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, WrapperPassThroughProps {
+  type?: 'text' | 'number' | 'email' | 'password';
+  className?: string;
 }
 
-// What about wrapper with div and label?
-function Input({ type = "text", className = "", ...rest}: Props){
-    return (
-        <input 
-            type={type} 
-            className={clsx(
-                "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5",
-                className
-            )}
-            {...rest}
-        />
-    )
+function Input({ type = 'text', className = '', labelValue, ...rest }: InputProps) {
+  return (
+    <FieldWrapper labelValue={labelValue}>
+      <input
+        type={type}
+        className={clsx(
+          'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5',
+          className,
+        )}
+        {...rest}
+      />
+    </FieldWrapper>
+  );
 }
 
 export default Input;
