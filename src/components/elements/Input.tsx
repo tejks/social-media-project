d6a8fd56fe11c6591c71d40048e5e7757067e3c7
tejks <sarmatys.programmer@gmail.com>
2023-11-08 13:04:41 +0100

    feat: Modify Input styles

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -12,7 +12,7 @@ function Input({ type = 'text', className = '', labelValue, ...rest }: InputProp
       <input
         type={type}
         className={clsx(
-          'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5',
+          'border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white',
           className,
         )}
         {...rest}
