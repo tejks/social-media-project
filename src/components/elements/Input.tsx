f78b024bd54850302aee3ad0266fc02b446fe7a9
jaugustyn <kub4a1@gmail.com>
2023-11-09 22:47:10 +0100

    feat: Update input with reacthookform register

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,20 +1,23 @@
 import clsx from 'clsx';
 import FieldWrapper, { WrapperPassThroughProps } from './FieldWrapper';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, WrapperPassThroughProps {
+  register: UseFormRegisterReturn;
   type?: 'text' | 'number' | 'email' | 'password';
   className?: string;
 }
 
-function Input({ type = 'text', className = '', labelValue, ...rest }: InputProps) {
+function Input({ type = 'text', className = '', labelValue, register, error, ...rest }: InputProps) {
   return (
-    <FieldWrapper labelValue={labelValue}>
+    <FieldWrapper labelValue={labelValue} error={error}>
       <input
         type={type}
         className={clsx(
           'border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white',
           className,
         )}
+        {...register}
         {...rest}
       />
     </FieldWrapper>
