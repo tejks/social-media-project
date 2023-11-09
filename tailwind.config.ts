c137a49e06d69748b1ee9e0b4050e5b36235fd3a
jaugustyn <kub4a1@gmail.com>
2023-11-09 22:46:18 +0100

    feat: Add even smaller margin (1px)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,10 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        '0.25': '1px',
+      }},
   },
   plugins: [],
 } satisfies Config;
