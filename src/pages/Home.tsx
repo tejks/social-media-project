74b7008694cff26592971e547b8aa001800df5be
tejks <sarmatys.programmer@gmail.com>
2023-12-20 12:30:02 +0100

    feat: Add a patterns on the background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,36 @@
-import image from '@assets/background.jpg';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import backgroundElement1 from '@assets/background-element-1.png';
+import backgroundElement4 from '@assets/background-element-4.jpg';
+import mainBackgroundImage from '@assets/background.jpg';
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className="flex h-screen flex-col justify-center">
+      <div className="absolute left-0 top-0 z-10 w-1/5 opacity-90 md:w-1/6">
+        <img src={backgroundElement1} alt="" width={250} />
+      </div>
+
+      <div className="absolute bottom-0 left-1/4 z-10 hidden opacity-60 xl:block">
+        <img src={backgroundElement4} alt="" width={450} />
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-10 w-1/5 rotate-180 opacity-90 md:hidden">
+        <img src={backgroundElement1} alt="" width={250} />
+      </div>
+
       <div className="grid items-center justify-items-center lg:grid-cols-2 lg:justify-items-end 2xl:mx-32">
         <div className="flex justify-center lg:col-start-2">
-          <img src={image} className="w-3/5 md:w-2/5 lg:w-2/3" alt="wolf-background" width={737} height={678} />
+          <img
+            src={mainBackgroundImage}
+            className="w-3/5 md:w-2/5 lg:w-2/3"
+            alt="wolf-background"
+            width={737}
+            height={678}
+          />
         </div>
 
         <div className="mt-16 flex w-5/6 flex-col items-center text-center lg:col-start-1 lg:row-start-1 lg:items-start lg:text-left">
