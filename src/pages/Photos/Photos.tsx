c382d2d754d74f58c0332a53614a920d4d4d05b5
jaugustyn <kub4a1@gmail.com>
2023-12-25 23:51:55 +0100

    feat: add image placeholders, used aliases in imports

--- src/pages/Photos/Photos.tsx
+++ src/pages/Photos/Photos.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import GalleryImage from './GalleryImage';
-import ImageModal from '../../components/ImageModal';
-import { IUnsplashPhoto } from '../../common/API/models/photo.model';
-import { useGetRandomPhotosQuery } from '../../common/API/services/photos';
+import { IUnsplashPhoto } from '@common/API/models/photo.model';
+import { useGetRandomPhotosQuery } from '@common/API/services/photos';
+import ScrollToTop from '@components/ScrollToTop';
+import ImageModal from '@components/ImageModal';
+import ImageSkeleton from '@components/elements/Skeleton/ImageSkeleton';
 
 const Photos: React.FC = () => {
   const { data, isLoading } = useGetRandomPhotosQuery();
@@ -28,23 +30,26 @@ const Photos: React.FC = () => {
   return (
     <>
       {isModalOpen === true && selectedImage !== null && <ImageModal onClose={closeModal} imageData={selectedImage} />}
-      <section id='photos-section' className="w-full m-auto p-4 columns-1 sm:columns-2 lg:columns-3 xl:columns-4 sm:w-5/6 lg:w-4/5 xl:w-3/4">
-        {photos && !isLoading ? (
-          photos?.map((item, index) => (
-            <GalleryImage
-              key={index}
-              src={item.urls.small}
-              alt={item.description}
-              width="100%"
-              hash={item.blur_hash}
-              onClick={() => openModal(item)}
-              className="mb-4 w-full rounded-xl"
-              loading="lazy"
-            />
-          ))
-        ) : (
-          <div>Not found</div>
-        )}
+      <section
+        id="photos-section"
+        className="container mx-auto pt-32 px-4 columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 sm:w-5/6 lg:w-4/5 2xl:w-3/4"
+      >
+        {photos && !isLoading
+          ? photos?.map((item, index) => (
+              <GalleryImage
+                key={index}
+                src={item.urls.small}
+                alt={item.description}
+                width="100%"
+                hash={item.blur_hash}
+                onClick={() => openModal(item)}
+                className="mb-4 w-full rounded-xl"
+                loading="lazy"
+              />
+            ))
+          : Array.from(Array(12).keys()).map((_, index) => <ImageSkeleton key={index} />)}
+
+        <ScrollToTop />
       </section>
     </>
   );
