f3419770f60135c2444cf2cb5383d572fe91805d
jaugustyn <kub4a1@gmail.com>
2023-12-25 22:36:55 +0100

    feat: add unsplash url to envSchema

--- src/common/config/env.ts
+++ src/common/config/env.ts
@@ -2,6 +2,7 @@ import * as z from 'zod';
 
 const envSchema = z.object({
   VITE_JSONPLACEHOLDER_URL: z.string(),
+  VITE_UNSPLASH_URL: z.string(),
 });
 
 export const env = envSchema.parse(import.meta.env);
