3c3109106d6de4cb2fb226b884cc5a2d35117e2e
tejks <sarmatys.programmer@gmail.com>
2023-12-29 17:00:35 +0100

    feat: Get the current user if logged in

--- src/common/store/authSlice.ts
+++ src/common/store/authSlice.ts
@@ -1,14 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { RootState } from '.';
 import { AuthUser } from '../API/models/user.model';
 import { authApi } from '../API/services/auth';
 
+const initialState = async (): Promise<{ user: AuthUser | null }> => {
+  try {
+    const user = await axios.get('http://localhost:3000/users/current', { withCredentials: true });
+    return { user: user.data as AuthUser };
+  } catch (error) {
+    return { user: null };
+  }
+};
+
 const authSlice = createSlice({
   name: 'auth',
-  initialState: { user: null, token: null } as {
-    user: AuthUser | null;
-    token: null | string;
-  },
+  initialState: await initialState(),
   reducers: {},
   extraReducers: (builder) => {
     builder.addMatcher(authApi.endpoints.signin.matchFulfilled, (state, { payload }) => {
@@ -16,7 +23,6 @@ const authSlice = createSlice({
     });
     builder.addMatcher(authApi.endpoints.signout.matchFulfilled, (state) => {
       state.user = null;
-      state.token = null;
     });
   },
 });
